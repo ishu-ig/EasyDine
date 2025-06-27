5eab2013b219eea26c7fc05e2f37ab7a611ecf5f
ishu-ig <ishaanguptacse@gmail.com>
2025-06-27 15:20:35 +0530

    first commit

--- server/db_connect.js
+++ server/db_connect.js
@@ -2,6 +2,7 @@ require("mongoose")
     .connect(process.env.DB_KEY)
     .then(() => {
         console.log("Dabata Base is Connected")
+        console.log(process.env.DB_KEY)
     })
     .catch(error => {
         console.log(error)
