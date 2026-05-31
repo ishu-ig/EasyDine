0ae939a89d4763dcd10bc345e1f36899195c5a3d
ishu-ig <ishaanguptacse@gmail.com>
2026-05-31 17:17:40 +0530

    updated project

--- client/src/Components/Products.jsx
+++ client/src/Components/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Product({ data, show }) {
+export default function Products({ data, show }) {
   const [displayedData, setDisplayedData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [itemsToShow, setItemsToShow] = useState(16);
